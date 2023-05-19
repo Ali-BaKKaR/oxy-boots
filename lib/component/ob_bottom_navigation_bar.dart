a4ffc7d91092174a6f6775873694353c5b7ddfc5
unknown <alibakar.ab14@gmail.com>
2023-05-19 16:01:49 +0300

    Fixing Bottom Navigation Bar

--- lib/component/ob_bottom_navigation_bar.dart
+++ lib/component/ob_bottom_navigation_bar.dart
@@ -14,72 +14,91 @@ class OBBottomNavigationBar extends StatefulWidget {
 }
 
 class OBBottomNavigationBarState extends State<OBBottomNavigationBar> {
-  int index = 0;
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> screens = <Widget>[
+    Home(),
+    Favourites(),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      shape: CircularNotchedRectangle(),
-      notchMargin: 10,
-      child: Container(
-          padding: EdgeInsets.only(left: 30, right: 30),
-          height: 70,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () => {
-                  index = 0,
-                  Navigator.of(context).pushNamed(
-                    Home.routeName,
-                  )
-                },
-                icon: SvgPicture.asset(
-                  'assets/icons/home.svg',
-                  width: 30,
-                  color: index == 0 ? Styles.PrimaryColor : Styles.TextColor,
-                ),
+    String? currentRouteName = ModalRoute.of(context)!.settings.name;
+    return Container(
+        padding: EdgeInsets.only(left: 30, right: 30),
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed(
+                  Home.routeName,
+                )
+              },
+              icon: SvgPicture.asset(
+                'assets/icons/home.svg',
+                width: 30,
+                color: currentRouteName == './home'
+                    ? Styles.PrimaryColor
+                    : Styles.TextColor,
               ),
-              IconButton(
-                onPressed: () => {
-                  index = 1,
-                  Navigator.of(context).pushNamed(
-                    Favourites.routeName,
-                  )
-                },
-                icon: Icon(
-                  Icons.favorite_border,
-                  size: 30,
-                  color: index == 1 ? Styles.PrimaryColor : Styles.TextColor,
-                ),
+            ),
+            IconButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed(
+                  Favourites.routeName,
+                )
+              },
+              icon: Icon(
+                Icons.favorite_border,
+                size: 30,
+                color: currentRouteName == './favourites'
+                    ? Styles.PrimaryColor
+                    : Styles.TextColor,
               ),
-              IconButton(
-                onPressed: () => {
-                  index = 2,
-                  Navigator.of(context).pushNamed(
-                    Favourites.routeName,
-                  )
-                },
-                icon: Icon(
-                  Icons.notifications_none,
-                  size: 30,
-                  color: index == 2 ? Styles.PrimaryColor : Styles.TextColor,
-                ),
+            ),
+            IconButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed(
+                  Favourites.routeName,
+                )
+              },
+              icon: Icon(
+                Icons.notifications_none,
+                size: 30,
+                color: currentRouteName == './notifications'
+                    ? Styles.PrimaryColor
+                    : Styles.TextColor,
               ),
-              IconButton(
-                onPressed: () => {
-                  index = 3,
-                  Navigator.of(context).pushNamed(
-                    Favourites.routeName,
-                  )
-                },
-                icon: Icon(
-                  Icons.person_outline,
-                  size: 30,
-                  color: index == 3 ? Styles.PrimaryColor : Styles.TextColor,
-                ),
-              )
-            ],
-          )),
-    );
+            ),
+            IconButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed(
+                  Favourites.routeName,
+                )
+              },
+              icon: Icon(
+                Icons.person_outline,
+                size: 30,
+                color: currentRouteName == './user'
+                    ? Styles.PrimaryColor
+                    : Styles.TextColor,
+              ),
+            )
+          ],
+        ));
   }
 }
